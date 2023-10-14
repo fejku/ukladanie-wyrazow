f6c0885b2e0b17b25fd68573ed3fd0a6d8017c61
fejku <przecinek45@o2.pl>
2023-10-14 13:05:44 +0200

    Start screen - title images

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -3,6 +3,8 @@ import { reset } from "../store/features/wordsSlice.ts";
 
 import playImg from "../assets/img/play.svg";
 import backgroundImg from "../assets/img/background-start-screen.jpg";
+import ukladanieWyrazowOneLineImg from "../assets/img/ukladanie_wyrazow_one_line.webp";
+import ukladanieWyrazowTwoLinesImg from "../assets/img/ukladanie_wyrazow_two_lines.webp";
 
 const StartScreen = () => {
   const dispatch = useAppDispatch();
@@ -16,10 +18,15 @@ const StartScreen = () => {
       className="h-screen flex justify-center items-center flex-col bg-no-repeat bg-cover bg-bottom"
       style={{ backgroundImage: `url(${backgroundImg})` }}
     >
-      <div className="flex flex-col basis-1/3 font-meatloaf text-8xl md:flex-row select-none">
-        <div className="md:mr-4">Układanie</div>
-        <div className="text-center">wyrazów</div>
-      </div>
+      <div
+        className={`flex basis-1/3 select-none w-9/12 bg-no-repeat bg-contain bg-top bg-[image:var(--bg-two-lines)] md:w-10/12 md:bg-[image:var(--bg-one-line)]`}
+        style={
+          {
+            "--bg-one-line": `url(${ukladanieWyrazowOneLineImg})`,
+            "--bg-two-lines": `url(${ukladanieWyrazowTwoLinesImg})`,
+          } as React.CSSProperties
+        }
+      ></div>
       <button
         className="w-24 h-24 animate-bounce md:w-36 md:h-36 hover:scale-150 hover:transition"
         onClick={onPlayClick}
